8c860187741e020da38a6adb1e0cde4c1bc56b28
Will Prater <will@superformula.com>
2019-04-02 09:43:05 -0700

    user Sf docker hub

--- build_docker.sh
+++ build_docker.sh
@@ -3,10 +3,11 @@
 set -e
 
 # pull to make sure we are not rebuilding for nothing
-docker pull cirrusci/flutter:base
+# docker push superformula/flutter:tagname
+docker pull superformula/flutter:base
 
-docker build --tag cirrusci/flutter:base base
-docker build --tag cirrusci/flutter:$FLUTTER_VERSION \
-             --tag cirrusci/flutter:latest \
+docker build --tag superformula/flutter:base base
+docker build --tag superformula/flutter:$FLUTTER_VERSION \
+             --tag superformula/flutter:latest \
              --build-arg flutter_version=$FLUTTER_VERSION \
              sdk
--- push_docker.sh
+++ push_docker.sh
@@ -1,14 +1,24 @@
 #!/usr/bin/env bash
 
+# Exit script if you try to use an uninitialized variable.
+set -o nounset
+
+# Exit script if a statement returns a non-true return value.
+set -o errexit
+
+# Use the error status of the first failure, rather than that of the last item in a pipeline.
+set -o pipefail
+
+# Exit script from a non-zero status
 set -e
 
-if [ "$CIRRUS_BRANCH" != "master" ]
+if [ "$CIRCLE_BRANCH" != "master" ]
 then
     exit 0
 fi
 
 docker login --username $DOCKER_USER_NAME --password $DOCKER_PASSWORD
 
-docker push cirrusci/flutter:base
-docker push cirrusci/flutter:$FLUTTER_VERSION
-docker push cirrusci/flutter:latest
+docker push superformula/flutter:base
+docker push superformula/flutter:$FLUTTER_VERSION
+docker push superformula/flutter:latest
